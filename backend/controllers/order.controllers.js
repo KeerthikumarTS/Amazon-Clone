f132b960bf1614be518bb8a7f045175f01d4e542
KeerthikumarTS <tskeerthikumar0504@gmail.com>
2024-02-03 19:07:42 +0530

    feat: crud operations related to ordering product implemented.

---
+++ backend/controllers/order.controllers.js
@@ -0,0 +1,111 @@
+import catchAsyncError from "../middlewares/catchAsyncError.js";
+import Order from "../models/order.model.js";
+import Product from "../models/product.model.js";
+
+export const newOrder =  catchAsyncError( async (req, res, next) => {
+    const {
+        orderItems,
+        shippingInfo,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+        paymentInfo
+    } = req.body;
+
+    const order = await Order.create({
+        orderItems,
+        shippingInfo,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+        paymentInfo,
+        paidAt: Date.now(),
+        user: req.user.id
+    })
+
+    res.status(200).json({
+        success: true,
+        order
+    })
+})
+
+export const getSingleOrder = catchAsyncError(async(req, res, next) => {
+
+    const order = await Order.findById(req.params.id).populate('user', 'name email');
+
+    if(!order){
+        return next(new ErrorHandler(`Order not found with this id: ${req.params.id}`, 404))
+    }
+
+    res.status(200).send({
+        success: true,
+        order
+    })
+})
+
+export const myOrders = catchAsyncError(async (req, res, next) => {
+    const orders = await Order.find({user: req.user.id});
+
+    res.status(200).json({
+        success: true,
+        orders
+    })
+})
+
+export const totalOrders = catchAsyncError(async (req, res, next) => {
+    const orders = await Order.find();
+
+    let totalAmount = 0;
+
+    orders.forEach(order => {
+        totalAmount += order.totalPrice
+    })
+
+    res.status(200).json({
+        success: true,
+        totalAmount,
+        orders
+    })
+})
+
+//Admin: Update Order / Order Status - api/v1/order/:id
+export const updateOrder =  catchAsyncError(async (req, res, next) => {
+    const order = await Order.findById(req.params.id);
+
+    if(order.orderStatus == 'Delivered') {
+        return next(new ErrorHandler('Order has been already delivered!', 400))
+    }
+    //Updating the product stock of each order item
+    order.orderItems.forEach(async orderItem => {
+        await updateStock(orderItem.product, orderItem.quantity)
+    })
+
+    order.orderStatus = req.body.orderStatus;
+    order.deliveredAt = Date.now();
+    await order.save();
+
+    res.status(200).json({
+        success: true
+    })
+    
+});
+
+async function updateStock (productId, quantity){
+    const product = await Product.findById(productId);
+    product.stock = product.stock - quantity;
+    product.save({validateBeforeSave: false})
+}
+
+//Admin: Delete Order - api/v1/order/:id
+export const deleteOrder = catchAsyncError(async (req, res, next) => {
+    const order = await Order.findByIdAndDelete(req.params.id);
+    if(!order) {
+        return next(new ErrorHandler(`Order not found with this id: ${req.params.id}`, 404))
+    }
+
+    res.status(200).json({
+        success: true
+    })
+})
---
+++ backend/routes/order.routes.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import { authorizeRoles, isAuthenticatedUser } from '../middlewares/authenticate.js';
+import { deleteOrder, getSingleOrder, myOrders, newOrder, totalOrders, updateOrder } from '../controllers/order.controllers.js';
+const router = express.Router();
+
+router.post('/order/new', isAuthenticatedUser, newOrder);
+router.get('/order/:id', isAuthenticatedUser, getSingleOrder);
+router.get('/myOrders', isAuthenticatedUser, myOrders);
+
+//Admin-routes:
+router.get('/admin/orders', isAuthenticatedUser, authorizeRoles('admin'), totalOrders);
+router.put('/admin/order/:id', isAuthenticatedUser, authorizeRoles('admin'), updateOrder)
+router.delete('/admin/order/:id', isAuthenticatedUser, authorizeRoles('admin'), deleteOrder)
+
+export default router;
