27bd6c96f61147e77b53bd7946a55ab8bdc0bc19
KeerthikumarTS <tskeerthikumar0504@gmail.com>
2024-02-07 23:22:39 +0530

    fix: changed config file from sever to app

--- backend/app.js
+++ backend/app.js
@@ -2,17 +2,29 @@ import express from "express";
 import productRoutes from './routes/product.routes.js'
 import authRoutes from './routes/auth.routes.js'
 import orderRoutes from './routes/order.routes.js'
+import paymentRoutes from './routes/payment.routes.js'
 import { errorMiddleware } from "./middlewares/error.js";
 import cookieParser from "cookie-parser";
+import path from 'path'
+import { fileURLToPath } from 'url'; 
+import dotenv from 'dotenv'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+dotenv.config({path:path.join(__dirname,'./config/.env')})
 
 const app = express();
 
 app.use(express.json())
 app.use(cookieParser())
+app.use('/uploads', express.static(path.join(__dirname,'uploads') ) )
 
 app.use('/api/v1',productRoutes)
 app.use('/api/v1',authRoutes)
 app.use('/api/v1', orderRoutes)
+app.use('/api/v1', paymentRoutes)
 
 app.use(errorMiddleware);
 
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,6 @@
 import app from "./app.js"
-import dotenv from 'dotenv'
-import path from 'path'
-import { fileURLToPath } from 'url'; 
 import { db } from "./config/db.js";
 
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-dotenv.config({path:path.join(__dirname,'./config/.env')})
 
 db();
 
